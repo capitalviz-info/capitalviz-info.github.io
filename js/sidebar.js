3c0f4b99985b362bbd342bc3a3eab25f5b7095f1
linhhoang <lthoang@wpi.edu>
2019-12-10 12:30:43 -0500

    fix tour

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,23 +6,27 @@ $(document).on("click", ".nav-pills .nav-item .nav-link", function (event) // Go
       case "#pills-overview":
         $("#lsidebar-overview").removeClass("none")
         $("#pills-tab > li:nth-child(1)").attr("style", "opacity: 1")
+        tour.cancel()
+
         break;
       case "#pills-funding":
         $("#lsidebar-funding").removeClass("none")
         $("#pills-tab > li:nth-child(1)").attr("style", "opacity: 0")
         if (firsttime) setTimeout(() => {
-          firsttime= false
+          firsttime = false
           tour.start()
-
         }, 500);
         break;
       case "#pills-acquisition":
         $("#lsidebar-acquisition").removeClass("none")
         $("#pills-tab > li:nth-child(1)").attr("style", "opacity: 1")
+        tour.cancel()
         break;
       default:
         $("#lsidebar-overview").removeClass("none")
         $("#pills-tab > li:nth-child(1)").attr("style", "opacity: 1")
+        tour.cancel()
+
 
     }
   });
